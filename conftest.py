cf0a7c3547530fae81fdc10a4f7c5f07b475e528
Ksenia Yarygina <ks.yarygina@yandex.ru>
2025-07-04 18:12:37 +0400

    Уязвимые данные вынесены в .env файл

--- conftest.py
+++ conftest.py
@@ -4,6 +4,16 @@ from selenium.webdriver.chrome.options import Options
 from selene.support.shared import browser
 
 from utils import attach
+from dotenv import load_dotenv
+import os
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
@@ -19,7 +29,7 @@ def setup_browser():
     options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}{selenoid_url}",
         options=options
     )
 
